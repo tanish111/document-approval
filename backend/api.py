a5aa60f92f819448bb1e8374de5ee33392121f0a
Pray Raskapoorwala <f20221239@goa.bits-pilani.ac.in>
2024-04-07 03:06:31 +0530

    version1

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from queries_sql import create_connection,get_psrn
+from queries_sql import create_connection,get_psrn,get_name,get_name_designation,get_ProjTitle_AgencyName,get_something,get_nfadet
 app = Flask(__name__)
 conn = create_connection()
 conn.is_connected()
@@ -14,6 +14,75 @@ def user_to_psrn():
     
     # Dummy user creation logic
     return get_psrn(conn,user_data["userEmail"])
+
+@app.route('/name', methods=['POST'])
+def psrn_to_name():
+    # Retrieve data from the request JSON payload
+    user_data = request.json
+    
+    # Assuming the request JSON payload contains 'userName', 'userEmail', and 'password' keys
+    if 'userPSRN' not in user_data:
+        return jsonify({'error': 'Missing required fields'}), 400
+    
+    # Dummy user creation logic
+    return get_name(conn,user_data["userPSRN"])
+
+@app.route('/name_designation', methods=['POST'])
+def psrn_to_name_designation():
+    # Retrieve data from the request JSON payload
+    user_data = request.json
+    
+    # Assuming the request JSON payload contains 'userName', 'userEmail', and 'password' keys
+    if 'userPSRN' not in user_data:
+        return jsonify({'error': 'Missing required fields'}), 400
+    
+    # Dummy user creation logic
+    return get_name_designation(conn,user_data["userPSRN"])
+
+@app.route('/ProjectTitle_AgencyName', methods=['POST'])
+def psrn_to_ProjectTitle_AgencyName():
+    # Retrieve data from the request JSON payload
+    user_data = request.json
+    
+    # Assuming the request JSON payload contains 'userName', 'userEmail', and 'password' keys
+    if 'userPSRN' not in user_data:
+        return jsonify({'error': 'Missing required fields'}), 400
+    
+    # Dummy user creation logic
+    return get_ProjTitle_AgencyName(conn,user_data["userPSRN"])
+
+@app.route('/something', methods=['POST'])
+def psrn_to_something():
+    # Retrieve data from the request JSON payload
+    user_data = request.json
+    
+    # Assuming the request JSON payload contains 'userName', 'userEmail', and 'password' keys
+    if 'userPSRN' not in user_data:
+        return jsonify({'error': 'Missing required fields'}), 400
+    
+    if 'userPROJID' not in user_data:
+        return jsonify({'error': 'Missing required fields'}), 400
+    
+    # Dummy user creation logic
+    return get_something(conn,user_data["userPSRN"],user_data["userPROJID"])
+
+@app.route('/nfadet', methods=['POST'])
+def psrn_to_nfadet():
+    # Retrieve data from the request JSON payload
+    user_data = request.json
+    
+    # Assuming the request JSON payload contains 'userName', 'userEmail', and 'password' keys
+    if 'userPSRN' not in user_data:
+        return jsonify({'error': 'Missing required fields'}), 400
+    
+    if 'userPANUM' not in user_data:
+        return jsonify({'error': 'Missing required fields'}), 400
+    
+    if 'userITEMID' not in user_data:
+        return jsonify({'error': 'Missing required fields'}), 400
+    
+    # Dummy user creation logic
+    return get_nfadet(conn,user_data["userPSRN"],user_data["userPANUM"],user_data["userITEMID"])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/queries_sql.py
+++ backend/queries_sql.py
@@ -6,7 +6,7 @@ def create_connection():
     server = 'localhost'
     database = 'ASCII'
     username = 'root'
-    password = 'Tanish1234'
+    password = 'root1234'
     try:
         conn = mysql.connector.connect(host=server, database=database, user=username, password=password)
         return conn
@@ -34,7 +34,142 @@ def get_psrn(conn, email):
             return jsonify({'error': 'Email not found'}), 404
 
         # Return PSRN
+        return jsonify(results), 200
+        
+    except mysql.connector.Error as e:
+        return jsonify({'error': f"Error connecting to MySQL Server: {e}"}), 500
+    
+    
+def get_name(conn, psrn):
+    try:
+        # Establish connection to MySQL Server if not already connected
+        if not conn.is_connected():
+            conn = create_connection()
+        cursor = conn.cursor()
+        
+        # Execute the query
+        sql = "SELECT Name FROM User_details WHERE PSRN = %s"
+        val = (psrn,)
+        cursor.execute(sql, val)        
+        results = cursor.fetchall()
+
+        # Close cursor
+        cursor.close()
+
+        # Check if the result is empty
+        if not results:
+            return jsonify({'error': 'psrn not found'}), 404
+
+        # Return PSRN
         return jsonify(results[0][0]), 200
         
+    except mysql.connector.Error as e:
+        return jsonify({'error': f"Error connecting to MySQL Server: {e}"}), 500
+    
+
+def get_name_designation(conn, psrn):
+    try:
+        # Establish connection to MySQL Server if not already connected
+        if not conn.is_connected():
+            conn = create_connection()
+        cursor = conn.cursor()
+        
+        # Execute the query
+        sql = "SELECT u.name,u.designation from User_Details u where u.psrn = %s"
+        val = (psrn,)
+        cursor.execute(sql, val)        
+        results = cursor.fetchall()
+
+        # Close cursor
+        cursor.close()
+
+        # Check if the result is empty
+        if not results:
+            return jsonify({'error': 'psrn not found'}), 404
+
+        # Return PSRN
+        return jsonify(results), 200
+    
+    except mysql.connector.Error as e:
+        return jsonify({'error': f"Error connecting to MySQL Server: {e}"}), 500
+    
+
+def get_ProjTitle_AgencyName(conn, psrn):
+    try:
+        # Establish connection to MySQL Server if not already connected
+        if not conn.is_connected():
+            conn = create_connection()
+        cursor = conn.cursor()
+        
+        # Execute the query
+        sql = "select p.ProjTitle,p.AgencyName from Projects p inner join NFABILLSR n on p.ProjID=n.Proj_ID and p.psrn = %s"
+        val = (psrn,)
+        cursor.execute(sql, val)        
+        results = cursor.fetchall()
+
+        # Close cursor
+        cursor.close()
+
+        # Check if the result is empty
+        if not results:
+            return jsonify({'error': 'psrn not found'}), 404
+
+        # Return PSRN
+        return jsonify(results), 200
+    
+    except mysql.connector.Error as e:
+        return jsonify({'error': f"Error connecting to MySQL Server: {e}"}), 500
+    
+
+def get_something(conn, psrn,projid):
+    try:
+        # Establish connection to MySQL Server if not already connected
+        if not conn.is_connected():
+            conn = create_connection()
+        cursor = conn.cursor()
+        
+        # Execute the query
+        sql = " select pl.SINum,pl.PartyName,pl.PONum,pl.BillNum,pl.BillAmt,pl.Advance from Proj_List pl inner join nfabillsr n on n.requestid=pl.requestid where n.psrn=%s and proj_id= %s"
+        val = (psrn,projid,)
+        cursor.execute(sql, val)        
+        results = cursor.fetchall()
+
+        # Close cursor
+        cursor.close()
+
+        # Check if the result is empty
+        if not results:
+            return jsonify({'error': 'psrn not found'}), 404
+
+        # Return PSRN
+        return jsonify(results), 200
+    
+    except mysql.connector.Error as e:
+        return jsonify({'error': f"Error connecting to MySQL Server: {e}"}), 500
+
+
+def get_nfadet(conn, psrn,panum,itemid):
+    try:
+        # Establish connection to MySQL Server if not already connected
+        if not conn.is_connected():
+            conn = create_connection()
+        cursor = conn.cursor()
+        
+        # Execute the query
+        sql = "SELECT ItemID, ItemDesc, InvoiceNum, Date1, ConferenceAmt, OtherAmt FROM NFAPA_List WHERE PSRN = %s AND PANum = %s AND ItemID = %s"
+        val = (psrn,panum,itemid,)
+        cursor.execute(sql, val)        
+        results = cursor.fetchall()
+
+        # Close cursor
+        cursor.close()
+
+        # Check if the result is empty
+        if not results:
+            return jsonify({'error': 'psrn not found'}), 404
+
+        # Return PSRN
+        return jsonify(results), 200
+    
     except mysql.connector.Error as e:
         return jsonify({'error': f"Error connecting to MySQL Server: {e}"}), 500
